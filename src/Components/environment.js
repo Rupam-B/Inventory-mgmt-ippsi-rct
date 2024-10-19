52f96d7ccb0a8d59c1d4f759fe6b19643eefe12a
Rupam-b <rupam.banerje@gmail.com>
2024-10-19 13:33:56 +0530

    table css updated3

--- src/Components/environment.js
+++ src/Components/environment.js
@@ -1,12 +1,12 @@
 // Production base URL
-// export const environment = {
-//     production: true,
-//     baseUrl: 'https://inventory-mgmt-ippsi-bcknd-sb-java.onrender.com'  
-// };
+export const environment = {
+    production: true,
+    baseUrl: 'https://inventory-mgmt-ippsi-bcknd-sb-java.onrender.com'  
+};
 
 // Development base URL
-export const environment = {
-    production: false,
-    baseUrl: 'http://localhost:8080'  
-  };
+// export const environment = {
+//     production: false,
+//     baseUrl: 'http://localhost:8080'  
+//   };
   
